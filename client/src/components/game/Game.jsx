fb74a9fe5c260ddda8f3318a5a9cde225a9de1f7
Elitsa Dimitrova <elitsa.v.dimitrova@protonmail.com>
2026-03-12 17:08:54 +0200

    Show dynamic details page

--- client/src/components/game/Game.jsx
+++ client/src/components/game/Game.jsx
@@ -1,16 +1,13 @@
 import { Link } from "react-router";
 
 export default function Game({ _id, genre, title, imageUrl }) {
-  {
-    console.log(1);
-  }
   return (
     <div className="game">
       <img src={imageUrl} alt={title} />
       <div className="details-overlay">
         <p className="name">{title}</p>
         <p className="genre">{genre}</p>
-        <Link to={`games/${_id}/details`} className="details-button">
+        <Link to={`/games/${_id}/details`} className="details-button">
           Details
         </Link>
       </div>
